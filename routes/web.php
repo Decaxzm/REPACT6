76badc5e3266ce5d7377206352059b2019c1dcf1
Decaxzm <barrfragan@gmail.com>
2025-04-02 18:02:08 -0500

    homework 5

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Creando las rutas necesarias.
+use App\Http\Controllers\ProductController;
+
+Route::resource(name: 'products', controller: ProductController::class);
